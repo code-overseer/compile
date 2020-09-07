9c1f8697ee6ac18b42c2df239f0e76d17777a145
muramasa2402 <wongjengyan@gmail.com>
2020-09-07 17:43:17 +0100

    add msvc demo

--- main.cpp
+++ main.cpp
@@ -8,22 +8,26 @@
 
 int main(int argc, char const* argv[])
 {
-    printf("Random string\n");
-    compile::for_<3>([&](auto i){ printf("%s\n", compile::StrGen<6>{}.value()); });
-    compile::for_<3>([&](auto i){ printf("%s\n", compile::StrGen<18>{}.value()); });
+    printf("Random string");
+    compile::for_<3>([&](auto i){ printf("\n%s", compile::StrGen<6>{}.value()); });
+    compile::for_<3>([&](auto i){ printf("\n%s", compile::StrGen<18>{}.value()); });
 
-    printf("\nEnsures compile time\n");
+    printf("\n\nEnsures compile time\n");
+    printf("%i\n", std::integral_constant<char, compile::StrGen<6>{}.value()[compile::IntGen<0>{}.value() % 6]>::value);
+#ifndef _MSC_VER
+    /* MSVC doesn't think the char const* is constexpr */
     constexpr char const* val{compile::StrGen<6>{}.value()};
     printf("%s\n", val);
-    compile::for_<6>([&](auto i){ printf("%i ", std::integral_constant<char, val[i.value]>{}.value); });
+    compile::for_<6>([&](auto i){ printf("%i ", std::integral_constant<char, val[i.value]>::value); });
+#endif
 
     printf("\n\nCounter\n");
-    printf("%i\n", std::integral_constant<int, compile::Counter<void>{}.increment()>{}.value);
-    printf("%i\n", std::integral_constant<int, compile::Counter<void>{}.increment()>{}.value);
-    printf("%i\n", std::integral_constant<int, compile::Counter<void>{}.increment()>{}.value);
-    printf("%i\n", std::integral_constant<int, compile::Counter<void>{}.increment()>{}.value);
-    printf("%i\n", std::integral_constant<int, compile::Counter<void>{}.increment()>{}.value);
-    printf("%i\n", std::integral_constant<int, compile::Counter<void>{}.increment()>{}.value);
+    printf("%i\n", std::integral_constant<int, compile::Counter<void>{}.increment()>::value);
+    printf("%i\n", std::integral_constant<int, compile::Counter<void>{}.increment()>::value);
+    printf("%i\n", std::integral_constant<int, compile::Counter<void>{}.increment()>::value);
+    printf("%i\n", std::integral_constant<int, compile::Counter<void>{}.increment()>::value);
+    printf("%i\n", std::integral_constant<int, compile::Counter<void>{}.increment()>::value);
+    printf("%i\n", std::integral_constant<int, compile::Counter<void>{}.increment()>::value);
 
 	return 0;
 }
